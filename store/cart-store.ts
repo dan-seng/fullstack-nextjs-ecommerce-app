3e6759b6c24c864b98f846234aa2c68ace806864
Daniel <da16gi@gmail.com>
2025-11-09 15:07:21 +0300

    final commit

--- store/cart-store.ts
+++ store/cart-store.ts
@@ -1,7 +1,7 @@
 import { create } from "zustand";
 import { persist } from "zustand/middleware";
 export interface CartItem {
-  images: any;
+  images?: any;
   id: string;
   name: string;
   price: number;
